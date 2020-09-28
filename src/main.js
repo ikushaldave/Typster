9c2ce9ad13578720e702f5b08ef13b1972ced162
Elton <celtonlloyd@gmail.com>
2020-09-28 21:14:12 +0530

    added default and worked on main ui

--- src/main.js
+++ src/main.js
@@ -2,17 +2,21 @@ const level = document.querySelector(".levels");
 const levelSelector = document.querySelector(".exercise-level-list");
 const ul = document.querySelector(".exercise-level-list");
 const writingText = document.querySelector(".writing-text");
+const active = document.querySelector(".active").textContent.toLowerCase();
 
 const grade = document.querySelector(".grade");
 const speed = document.querySelector(".speed");
 const time = document.querySelector(".time");
 const wordLeft = document.querySelector(".word-left");
-const mistakes = document.querySelector(".error");
-
+const error = document.querySelector(".error");
+let latest = null;
 function levelHandler(e) {
 	if (e.target.classList.contains("level-btn")) {
 		let targeted = e.target.innerText.toLowerCase();
 		carouselUi(levels[targeted].length, targeted);
+		[...level.children].forEach(e =>
+			e.classList.remove("active"));
+		e.target.classList.add("active");
 	}
 }
 // carousel ui function
@@ -43,13 +47,18 @@ function carouselUi(len, lvl) {
 // carousel handler function
 function carouselHandler(e) {
 	if (!e.target.classList.contains("locked") && e.target.tagName == "BUTTON") {
+		console.log(e.target.children[0].innerText);
+		let index = +(e.target.children[0].innerText)-1;
+		createUi(levels[active][index].typing);
 	}
+
 }
 
-function createUi(writingTest, t, w, m, g = 0, s = 0) {
+function createUi(writingTest, t, w, er, g = 0, s = 0) {
+	console.log("working");
 	writingText.innerText = writingTest;
 	time.innerText = t;
-	mistakes.innerText = m;
+	error.innerText = er;
 	wordLeft.innerText = writingTest.split(" ").length;
 	for (let i = 0; i < g; i++) {
 		grade.children[i].classList.remove("fa-star").add("fa-star-o");
@@ -59,4 +68,15 @@ function createUi(writingTest, t, w, m, g = 0, s = 0) {
 
 level.addEventListener("click", levelHandler);
 
-carouselUi(levels.beginner.length, "beginner");
+carouselUi(levels[active].length, active);
+
+function latestIndex() {
+	
+	levels[active].forEach((ele, i) => {
+		if (ele.isCompleted ) {
+			latest = levels[active][i+1];
+		}
+	})
+}
+latestIndex();
+createUi(latest.typing);
